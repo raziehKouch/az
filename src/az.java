cb44bd1a9bad339de15def8cda2c70a43621dc99
rkouchakzadeh <rkouchakzadeh@ce.sharif.edu>
2016-12-08 17:50:18 +0330

    hazf shod

--- src/az.java
+++ src/az.java
@@ -4,7 +4,6 @@
 public class az {
     public static void main(String[] args) {
         azc m = new azc();
-        System.out.println("1.2.3.emtehan mikonim! :)");
         System.out.println("1.2.3.emtehan mikonim! dobareeee :)");
     }
 }
